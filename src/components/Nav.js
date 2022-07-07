23bce4ba65abaac72684e6f87de528a100bb216f
MG_Apps <99666081+mgdeveloper1406@users.noreply.github.com>
2022-07-07 09:07:59 -0300

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ const Nav = ({ setLibraryStatus, libraryStatus }) => {
   
     return (
       <nav>
-        <h1>Lección de esta semana</h1>
+        <h1>Radios</h1>
         <button
           className={libraryStatus ? "library-active" : ""}
           onClick={openLibraryHandler}
